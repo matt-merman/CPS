f9fdda2a5e934c44947decf79a22b0918d226a65
matt-marman <geom.dibattistamattia@gmail.com>
2022-01-28 23:17:32 +0100

    Ready to delivery

--- README.md
+++ README.md
@@ -1,1 +1,76 @@
-# FRED
+# FRED (Federal Reserve Economic Data)
+
+## Specifica
+
+L'obiettivo è definire un package in Python per interagire con le API messe a disposizione da [FRED](https://fred.stlouisfed.org/) (sito fornitore di dati economici, organizzati in categorie). 
+
+## Codice
+
+L'applicazione è stata sviluppata in Jupyter, per poi esserne creato il relativo package (disponibile a [www.pypi.com](https://pypi.org/project/fred-matt-merman/)). Il codice scritto è dunque lo stesso, cambia solo l'organizzazione. Di seguito sono riportati i file, con i metodi implementati:
+
+_request.py_
+
+```python
+request_json(url);
+#Effettua una richiesta di GET, sfruttando la libreria 'requests', e ritorna il file .json.
+
+get(url_base, url_key, type, id);
+#Impacchetta l'input da passare a request_json(), come la chiave per l'API e l'identificativo di ciò che si vuole scaricare (e.g. series, category).
+```
+
+_database.py_
+
+```python
+connect_db(db_file);
+#Ritorna l'istanza di connessione ad un db SQLite locale, prendendo come parametro il nome del db (e.g. 'database.db').
+
+create_db(db_file);
+#Crea il db SQLite locale, con 4 tabelle: categories, seriess, observations, seriess_downloaded.
+
+insert_db(db_file, id, type, update);
+#Permette il popolamento della tabelle contenute nel db. Il parametro update consente di aggiornare le entries qualora queste fossero già presenti.
+
+get_db(db_file, type, id);
+#Ritorna il dataframe corrispondente alla tabella che si vuole vedere (i.e. categories, seriess, observations, seriess_downloaded).
+
+download_insert_tree_category(db_file, id, type, update, array);
+#Fornendo un id da cui partire, sono scaricate ricorsivamente tutte le sottocategorie figlie. Il metodo è molto lento, soprattutto se fornito un id 'alto', come lo 0. Per evitare di saturare il server FRED è stato inserito un timer, di 0.5 secondi tra due chiamate.
+```
+
+_display.py_
+
+```python
+convert_string_float(array_string);
+#Operazione necessaria nel calcolo della media mobile.
+
+display(x, y, title, id, x_label);
+#Effettua il setting dei grafici da ritornare.
+
+display_observation(id);
+#Ritorna l'oggetto plt da visualizzare, basato sull'id della serie passata.
+
+moving_average(input_array, day);
+#Calcola la media mobile.
+
+display_moving_average(id, day);
+#Ritorna l'oggetto plt da visualizzare, basato sull'id della serie passata.
+```
+
+L'implementazione del case study richiesto (_Money, Banking, & Finance > Exchange Rates > Daily Rates_), è disponibile nel file _user.py_. 
+
+## Installazione
+
+Per installare il package:
+
+```bash
+pip install fred-matt-merman
+```
+
+Sono inoltre necessarie le librerie esterne:
+
+* numpy;
+* requests;
+* pandas;
+* matplotlib;
+
+
---
+++ code/user.py
@@ -0,0 +1,215 @@
+import fred_matt_merman as fmm
+from matplotlib import pyplot as plt
+import numpy as np
+import pandas as pd
+
+#CASE STUDY (Money, Banking, & Finance > Exchange Rates)
+ID_CATEGORY = 94 #Daily Rates
+ID_SERIES = ["DEXBZUS", "DEXCAUS", "DEXCHUS"]
+DB = "./database.db"
+
+#costante usata per selezionare il numero di valori da graficare.
+#è importante che sia negativa! In quanto sono graficati gli ultimi dati
+#è usato anche nel calcolo della varinza tra serie, nel case study
+NUMBER_VALUE = -50
+
+grid_color = "grey"
+label_color = "white"
+plot_color="green"
+title_fontsize = 30
+axes_fontsize = 20
+text_pad = 20
+context = {'axes.edgecolor':'grey',
+           'axes.facecolor':'black',
+           'font.family':'sans-serif', 
+           'figure.facecolor':'black', 
+           'figure.edgecolor':'black',
+           'xtick.color':'white', 
+           'ytick.color':'white', 
+           'savefig.transparent':'True'}
+
+def get_observations(list_series):
+
+    if isinstance(list_series, list) and len(list_series) == 3:
+
+        df = fmm.get_db(DB, "observations", list_series[0])
+        observations_1 = df['date'].tolist()
+
+        observations_2 = df['value'].tolist()
+        observations_2 = fmm.convert_string_float(observations_2)
+
+        df = fmm.get_db(DB, "observations", list_series[1])
+        observations_3 = df['value'].tolist()
+        observations_3 = fmm.convert_string_float(observations_3)
+
+        df = fmm.get_db(DB, "observations", list_series[2])
+        observations_4 = df['value'].tolist()
+        observations_4 = fmm.convert_string_float(observations_4)
+
+        return (observations_1, observations_2, observations_3, observations_4)
+    
+    else:
+        
+        df = fmm.get_db(DB, "observations", list_series)
+        observations_1 = df['date'].tolist()
+        observations_2 = df['value'].tolist()
+        observations_2 = fmm.convert_string_float(observations_2)
+        
+        return (observations_1, observations_2)
+        
+def display_observations(list_series):
+    
+    observations = get_observations(list_series)
+
+    with plt.rc_context(context):
+  
+        plt.figure(figsize=(21,9))
+        
+        plt.title("Observations", color=label_color, pad=text_pad, fontsize=title_fontsize)
+        plt.grid(color=grid_color)
+        
+        plt.plot(observations[0][NUMBER_VALUE:], observations[1][NUMBER_VALUE:], color="blue", label="ID: " + list_series[0])
+        plt.plot(observations[0][NUMBER_VALUE:], observations[2][NUMBER_VALUE:], color="green", label="ID: " + list_series[1])
+        plt.plot(observations[0][NUMBER_VALUE:], observations[3][NUMBER_VALUE:], color="orange", label="ID: " + list_series[2])
+    
+        plt.ylabel("value", color=label_color, fontsize=axes_fontsize, labelpad=text_pad)
+        plt.xlabel("date", color=label_color, fontsize=axes_fontsize, labelpad=text_pad)
+        plt.xticks(rotation=45)
+        
+        #mp.savefig('chart_observation.png')
+        
+        legend = plt.legend(fontsize=text_pad)
+        plt.setp(legend.get_texts(), color=label_color)
+    
+        return plt
+    
+#Calcolare la covarianza
+def covariance(list_series):
+    
+    observations = get_observations(list_series)
+    
+    covariance = np.cov(observations[1][NUMBER_VALUE:], observations[2][NUMBER_VALUE:])[0][1]
+    print("Cov(" + list_series[0] + "," + list_series[1] + ") = " + str(covariance))
+    
+    covariance = np.cov(observations[3][NUMBER_VALUE:], observations[2][NUMBER_VALUE:])[0][1]
+    print("Cov(" + list_series[2] + "," + list_series[1] + ") = " + str(covariance))
+    
+    covariance = np.cov(observations[1][NUMBER_VALUE:], observations[3][NUMBER_VALUE:])[0][1]
+    print("Cov(" + list_series[0] + "," + list_series[2] + ") = " + str(covariance))
+    
+#Calcolare e graficare su un solo grafico per ogni serie
+#la serie delle differenze prime (𝑠𝑖+1 −𝑠𝑖) 
+def display_first_diff(id):
+    
+    observations = get_observations(id)
+    
+    difference_array = []
+    for index in range(len(observations[1]) - 1):
+        
+        difference = observations[1][index + 1] - observations[1][index]
+        difference_array.append(difference)
+    
+    return fmm.display(observations[0], difference_array, "First differences", id, "date")
+
+#Calcolare e graficare su un solo grafico per ogni serie la serie delle 
+#differenze prime percentuali 𝑠𝑖+1−𝑠𝑖. (fate attenzione agli ∞)
+def display_first_diff_perc(id):
+
+    observations = get_observations(id)
+    
+    ratio_array = []
+    for index in range(len(observations[1]) - 1):
+        
+        if observations[1][index] == 0: observations[1][index] = 0.1
+        difference = (observations[1][index + 1] - observations[1][index])/observations[1][index]
+        ratio_array.append(difference)
+    
+    return fmm.display(observations[0], ratio_array, "First differences percentages", id, "date")
+
+def estimate_coef(x, y):
+    
+    # number of observations/points
+    n = np.size(x)
+ 
+    # mean of x and y vector
+    m_x = np.mean(x)
+    m_y = np.mean(y)
+ 
+    # calculating cross-deviation and deviation about x
+    SS_xy = np.sum(y*x) - n*m_y*m_x
+    SS_xx = np.sum(x*x) - n*m_x*m_x
+ 
+    # calculating regression coefficients
+    b_1 = SS_xy / SS_xx
+    b_0 = m_y - b_1*m_x
+ 
+    return (b_0, b_1)
+    
+#Per ogni serie calcolare la retta di regressione
+def display_linear_regression(id):
+        
+    df = fmm.get_db(DB, "observations", id)
+    
+    #converte la sequenza di float in int
+    for index in range(len(df)):
+        value = df['value'][index]
+        if value == ".": 
+            df.iat[index, 2] = 0
+ 
+    df['value'] = pd.to_numeric(df['value']).astype(int)
+    df['day'] = range(1, len(df) + 1)
+    
+    b = estimate_coef(df['day'], df['value'])
+            
+    with plt.rc_context(context):
+  
+        plt.figure(figsize=(21,9))
+        
+        plt.title("Linear regression", color=label_color, pad=text_pad, fontsize=title_fontsize)
+        plt.grid(color=grid_color)
+        
+        plt.scatter(df['day'].tolist()[NUMBER_VALUE:], df['value'].tolist()[NUMBER_VALUE:], color = "m", marker = "o", s = 30, label="ID: " + id)
+
+        # predicted response vector
+        y_pred = b[0] + b[1]*df['day']
+
+        # plotting the regression line
+        plt.plot(df['day'].tolist()[NUMBER_VALUE:], y_pred.tolist()[NUMBER_VALUE:], color = "b")
+
+        plt.ylabel("value", color=label_color, fontsize=axes_fontsize, labelpad=text_pad)
+        plt.xlabel("day", color=label_color, fontsize=axes_fontsize, labelpad=text_pad)
+        plt.xticks(rotation=45)
+        
+        #mp.savefig('chart_observation.png')
+        
+        legend = plt.legend(fontsize=text_pad)
+        plt.setp(legend.get_texts(), color=label_color)
+    
+        return plt
+    
+#Scaricare  tutte le serie  da  una  singola categoria (in realtà ne sono sufficienti 3)
+#potrebbe essere necessario creare un db, con fmm.create_db(DB)
+fmm.insert_db(DB, ID_CATEGORY, "series", False)
+
+#Per vedere tutte le seriess scaricate, e quindi anche quelle relative a Daily Rates:
+df = fmm.get_db(DB, "seriess", None)
+
+#Scaricare tutte le osservabili per 3 seriess scelte
+for index in range(len(ID_SERIES)):
+    fmm.insert_db(DB, ID_SERIES[index], "observation", False)
+
+#Graficare le 3 serie su un solo grafico 
+plt = display_observations(ID_SERIES)
+plt.show()
+
+#Calcolare la covarianza (attenzione ai nan!) 
+covariance(ID_SERIES)
+
+#Serie delle differenze prime, differenze prime percentuali e retta di regressione
+for index in range(len(ID_SERIES)):
+    plt = display_first_diff(ID_SERIES[index])
+    plt.show()
+    plt = display_first_diff_perc(ID_SERIES[index])
+    plt.show()
+    plt = display_linear_regression(ID_SERIES[index])
+    plt.show()
